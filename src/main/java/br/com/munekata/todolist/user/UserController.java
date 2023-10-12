dbcc3a1ace51a7e5b224902dc1277370d1a8598b
Erick <erick.munekata@gmail.com>
2023-10-12 18:08:55 -0300

    First commit

---
+++ src/main/java/br/com/munekata/todolist/user/UserController.java
@@ -0,0 +1,31 @@
+package br.com.munekata.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity CreateUser(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var createdUser = this.userRepository.save(userModel);
+        System.out.println("User " + createdUser.getName() + " Created successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+}
---
+++ src/test/java/br/com/munekata/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.munekata.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
